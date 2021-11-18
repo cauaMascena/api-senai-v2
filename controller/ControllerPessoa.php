93110a38a6ff43382febb0ff67ad1adaff5bf78b
cauaMascena <cauasinho656@gmail.com>
2021-11-18 11:29:07 -0300

    tird commit

--- controller/ControllerPessoa.php
+++ controller/ControllerPessoa.php
@@ -5,7 +5,22 @@ class ControllerPessoa{
     private $_method;
     private $_modelPessoa;
 
-    public function __
+    public function __construct($model){
+        
+        $this->modelPessoa = $model;
+        $this->method = $_SERVER['REQUEST_METHOD'];
+    }
+    function router(){
+        switch ($variable) {
+            case 'value':
+                # code...
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
 }
 
 
